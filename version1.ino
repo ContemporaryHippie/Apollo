03c474029b98b4fcd9ff9af1571ef3aac783d2fc
JamesMatherly <jlm13d@my.fsu.edu>
2016-03-12 21:20:14 -0500

    Added files via upload
    Added uSpeech library and rudimentary open function for motor

--- version1.ino
+++ version1.ino
@@ -1,3 +1,5 @@
+#include <uspeech.h> //speech recognition. will be used to close curtains
+
 #include <EthernetServer.h>
 #include <util.h>
 #include <Dhcp.h>
@@ -24,13 +26,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Ethernet.begin(mac,192.168.1.5); //mac is to be the mac address of the shield
+  
   setSyncProvider(getNtpTime);
   setSyncInterval(3600);
-  pinMode(motor, OUTPUT);
-  /*Alarm.alarmRepeat(dowMonday,8,30,00,MoCurt); //curtains at 8:30 Monday
+  
+  pinMode(motor, OUTPUT); //motor directional component will go in this block
+  
+  Alarm.alarmRepeat(dowMonday,8,30,00,MoCurt); //curtains at 8:30 Monday
   Alarm.alarmRepeat(dowTuesday,8,30,00,TuCurt); //curtains at 8:30 Tuesday
   Alarm.alarmRepeat(dowWednesday,10,00,00,WeCurt); //curtains at 10:00 Wednesday
-  Alarm.alarmRepeat(dowThursday,8,30,00,ThCurt); //curtains at 8:30 Thursday*/
+  Alarm.alarmRepeat(dowThursday,8,30,00,ThCurt); //curtains at 8:30 Thursday
   Alarm.alarmRepeat(dowThursday,9,30,00,Test); //test case
 
 }
@@ -38,29 +43,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly: 
   
-  digitalWrite(motor, HIGH);
-  delay(5000);
-  digitalWrite(motor, LOW);
+
   
 }
-/*
+
 void MoCurt(){
-  Serial.println("Alarm: - turn lights off");    
+  digitalWrite(motor, HIGH); //rudimentary motor activation. will improve with h circuit and appropriate pin activation
+  delay(5000);
+  digitalWrite(motor, LOW);  
 }
 
 void TuCurt(){
-  Serial.println("Alarm: - turn lights off");    
+  digitalWrite(motor, HIGH); //rudimentary motor activation. will improve with h circuit and appropriate pin activation
+  delay(5000);
+  digitalWrite(motor, LOW);   
 }
 
 void WeCurt(){
-  Serial.println("Alarm: - turn lights off");    
+  digitalWrite(motor, HIGH); //rudimentary motor activation. will improve with h circuit and appropriate pin activation
+  delay(5000);
+  digitalWrite(motor, LOW);   
 }
 
 void ThCurt(){
-  Serial.println("Alarm: - turn lights off");    
-}*/
+  digitalWrite(motor, HIGH); //rudimentary motor activation. will improve with h circuit and appropriate pin activation
+  delay(5000);
+  digitalWrite(motor, LOW);   
+}
 
 void Test(){
-  Serial.println("Alarm: - turn lights off");    
+  digitalWrite(motor, HIGH); //rudimentary motor activation. will improve with h circuit and appropriate pin activation
+  delay(5000);
+  digitalWrite(motor, LOW);   
 }
 
